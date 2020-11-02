c3e2f60af62fc579707f0a9cd8bd892df85f591f
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2020-11-02 14:24:49 -0300

    reducing travis findings

--- src/test/java/edu/ifrs/vvs/BubbleSortTest.java
+++ src/test/java/edu/ifrs/vvs/BubbleSortTest.java
@@ -2,31 +2,45 @@ package edu.ifrs.vvs;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;;
 
+/**
+ * Unit test for simple App.
+ */
 public class BubbleSortTest {
-    BubbleSort algoritm = new BubbleSort();
+    /**
+     * Testing if the numbers were sorted
+     */
     @Test
     void numberSorted(){
-        int [] initial = {5, 3, 1};   
+        BubbleSort algoritm = new BubbleSort();
+        int[] initial = {5, 3, 1};   
         algoritm.sort(initial);
-        int [] expected = {1, 3, 5};   
+        int[] expected = {1, 3, 5};   
         assertArrayEquals(expected, initial);    
     }    
 
+    /**
+     * Testing if negative number were sorted
+     */
     @Test
     void numberSortedWithNegativeNumber(){
-        int [] initial = {-5, 3, 1};
+        BubbleSort algoritm = new BubbleSort();
+        int[] initial = {-5, 3, 1};
         algoritm.sort(initial);
-        int [] expected = {-5, 1, 3};   
+        int[] expected = {-5, 1, 3};   
         assertArrayEquals(expected, initial);  
     }        
 
+    /**
+     * Testing if repeated numbers were considered
+     */
     @Test
     void numberSortedWithRepeatedNumber(){
-        int [] initial = {-15, 10, 10, 2, 2};
+        BubbleSort algoritm = new BubbleSort();
+        int[] initial = {-15, 10, 10, 2, 2};
         algoritm.sort(initial);
-        int [] expected = {-15, 2, 2, 10, 10};   
+        int[] expected = {-15, 2, 2, 10, 10};   
         assertArrayEquals(expected, initial);  
     }         
 }
