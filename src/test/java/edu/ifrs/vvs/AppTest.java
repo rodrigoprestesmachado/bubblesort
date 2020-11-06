a7bfd465c90ad261539841e16c668c33e954b720
guismoreira <guilhermemoreira55@gmail.com>
2020-11-05 22:35:33 -0300

    update class AppTest

--- src/test/java/edu/ifrs/vvs/AppTest.java
+++ src/test/java/edu/ifrs/vvs/AppTest.java
@@ -1,35 +1,48 @@
-/**
- * @License
- * Copyright 2020 Bubble Sort Application
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package edu.ifrs.vvs;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
- * Unit test for simple App.
+ * bubble test.
  */
-class AppTest {
-    /**
-     * Rigorous Test.
-     */
+public class AppTest {
+
+    private BubbleSort bubble = new BubbleSort();
+
+    @SuppressWarnings("checkstyle:magicnumber")
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testVet() {
+        int[] num = {10};
+        bubble.sort(num);
+        assertArrayEquals(new int[] {10}, num);
+    }
+
+    @SuppressWarnings("checkstyle:magicnumber")
+    @Test
+    void testVetOrder() {
+        int[] num = {8, 6, 9};
+        int[] order = {6, 8, 9};
+        bubble.sort(num);
+        assertArrayEquals(order, num);
+    }
+
+    @SuppressWarnings("checkstyle:magicnumber")
+    @Test
+    void testVetNull() {
+        int[] num = {};
+        int[] order = {};
+        bubble.sort(num);
+        assertArrayEquals(order, num);
+    }
+
+    @SuppressWarnings("checkstyle:magicnumber")
+    @Test
+    void testVetNegative() {
+        int[] num = {-6, -2, -8};
+        int[] order = {-8, -6, -2};
+        bubble.sort(num);
+        assertArrayEquals(order, num);
     }
 }
