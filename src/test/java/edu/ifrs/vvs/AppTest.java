1e4cdc6ec6b4f1c32e0d99e54429906a27464690
mariohood <manhattan199@hotmail.com>
2020-11-23 19:43:10 -0300

    se incremento o testVetOrder

--- src/test/java/edu/ifrs/vvs/AppTest.java
+++ src/test/java/edu/ifrs/vvs/AppTest.java
@@ -19,17 +19,30 @@ package edu.ifrs.vvs;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
+
+    private BubbleSort bubble = new BubbleSort();
     /**
      * Rigorous Test.
      */
+
     @Test
     void testApp() {
         assertEquals(1, 1);
+    }
+
+    @SuppressWarnings("checkstyle:magicnumber")
+    @Test
+    void testVetOrder() {
+        int[] inicial = {8, 6, 2, 9};
+        int[] order = {2, 6, 8, 9};
+        bubble.sort(inicial);
+        assertArrayEquals(order, inicial);
     }
 }
