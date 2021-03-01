95098f32213b9e82353589abc295d9eb16ac3161
Gabriel Lima <gabriel.lima.0096@gmail.com>
2021-03-01 01:03:22 +0000

    Ignore magic numbers and fix white spaces

--- src/test/java/edu/ifrs/vvs/BubbleSortTest.java
+++ src/test/java/edu/ifrs/vvs/BubbleSortTest.java
@@ -7,14 +7,16 @@ public class BubbleSortTest {
     private BubbleSort bubblesort = new BubbleSort();
 
     @Test
+    @SuppressWarnings("checkstyle:magicnumber")
     public void sortWithRandomNumbers() {
-        int[] integerArray = new int[] { 5, 9, 2, 8, 5, -4, 1, 0, 25 };
-        int[] sortedArray = new int[] { -4, 0, 1, 2, 5, 5, 8, 9, 25 };
+        int[] integerArray = new int[] {5, 9, 2, 8, 5, -4, 1, 0, 25};
+        int[] sortedArray = new int[] {-4, 0, 1, 2, 5, 5, 8, 9, 25};
         bubblesort.sort(integerArray);
         Assertions.assertArrayEquals(integerArray, sortedArray);
     }
 
     @Test
+    @SuppressWarnings("checkstyle:magicnumber")
     public void sortWithEmptyArray() {
         int[] integerArray = new int[] {};
         int[] sortedArray = new int[] {};
