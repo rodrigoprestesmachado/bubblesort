78325289fc3a421aaacdb5bd89ee8ffdc33b312f
Vitor Rosa <58368306+VitorSdaRosa@users.noreply.github.com>
2021-03-01 12:50:01 -0300

    Test Bubblesort

--- src/test/java/edu/ifrs/vvs/AppTest.java
+++ src/test/java/edu/ifrs/vvs/AppTest.java
@@ -19,17 +19,44 @@ package edu.ifrs.vvs;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
- * Unit test for simple App.
+ * Unit test for Bubblesort.
  */
 class AppTest {
+
+    private BubbleSort bubbleSort = new BubbleSort();
+
     /**
      * Rigorous Test.
      */
+    
+    @SuppressWarnings("checkstyle:magicnumber")
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testAppVetor() {
+        int[] valores = { 10 };
+        bubbleSort.sort(valores);
+        assertArrayEquals(new int[] { 10 }, valores);
     }
+
+    @SuppressWarnings("checkstyle:magicnumber")
+    @Test
+    void testAppVetorNull() {
+        int[] valores = {};
+        int[] ordemEsperada = {};
+        bubbleSort.sort(valores);
+        assertArrayEquals(ordemEsperada, valores);
+    }
+    
+    @SuppressWarnings("checkstyle:magicnumber")
+    @Test
+    void testAppVetorOrdem() {
+        int[] valores = {108, 88, 18, 57, -2};
+        int[] ordemEsperada = {-2, 18, 57, 88, 108};
+        bubbleSort.sort(valores);
+        assertArrayEquals(ordemEsperada, valores);
+    }
+
+
 }
