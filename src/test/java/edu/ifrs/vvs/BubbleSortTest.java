3e62f7e156486c934f265021f601a94888974969
Henrique Kalife <henriquekalife@gmail.com>
2021-02-28 21:28:41 -0300

    refactor(bubblesort): remove unused imports

--- src/test/java/edu/ifrs/vvs/BubbleSortTest.java
+++ src/test/java/edu/ifrs/vvs/BubbleSortTest.java
@@ -3,8 +3,6 @@ package edu.ifrs.vvs;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.Random;
-import java.util.ArrayList;
 
 class AppTest {
 
