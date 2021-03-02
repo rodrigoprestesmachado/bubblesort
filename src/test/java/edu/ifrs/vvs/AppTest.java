05c2fbf93465d4cbfae3fe494300ce0644d7073b
pameladilly <pameladilly@hotmail.com>
2021-03-02 19:44:28 -0300

    Implementação de testes de ordenação

--- src/test/java/edu/ifrs/vvs/AppTest.java
+++ src/test/java/edu/ifrs/vvs/AppTest.java
@@ -17,6 +17,8 @@
 
 package edu.ifrs.vvs;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,8 +30,87 @@ class AppTest {
     /**
      * Rigorous Test.
      */
+
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    @DisplayName("Valores positivos")
+    void testValoresPositivos(){
+
+        BubbleSort bs = new BubbleSort();
+
+        int[] valores = new int[3];
+
+        valores[0] = 1;
+        valores[1] = 9;
+        valores[2] = 4;
+
+        bs.sort(valores);
+
+        assertEquals(valores[0], 1  );
+        assertEquals(valores[1], 4  );
+        assertEquals(valores[2], 9  );
+    }
+
+    @Test
+    @DisplayName("Valores negativos e iguais")
+    void testeValoresNegativosEIguais(){
+
+        BubbleSort bs = new BubbleSort();
+
+        int[] valores = new int[7];
+
+        valores[0] = -1;
+        valores[1] = -981;
+        valores[2] = -3;
+        valores[3] = -9281739;
+        valores[4] = -9281738;
+        valores[5] = -1;
+        valores[6] = -1;
+
+        bs.sort(valores);
+
+        assertEquals(valores[0], -9281739  );
+        assertEquals(valores[1], -9281738  );
+        assertEquals(valores[2], -981  );
+        assertEquals(valores[3], -3  );
+        assertEquals(valores[4], -1  );
+        assertEquals(valores[5], -1 );
+        assertEquals(valores[6], -1 );
+
+
+    }
+
+    @Test
+    @DisplayName("Valores negativos, iguais e zero")
+    void testeValoresNegativosIguaisEZero(){
+
+
+        BubbleSort bs = new BubbleSort();
+
+        int[] valores = new int[9];
+
+        valores[0] = -1;
+        valores[1] = -981;
+        valores[2] = -3;
+        valores[3] = 0;
+        valores[4] = -9281738;
+        valores[5] = -1;
+        valores[6] = -1;
+        valores[7] = 9875555;
+        valores[8] = 9875551;
+
+
+        bs.sort(valores);
+
+        assertEquals(valores[0], -9281738  );
+        assertEquals(valores[1], -981  );
+        assertEquals(valores[2], -3  );
+        assertEquals(valores[3], -1  );
+        assertEquals(valores[4], -1 );
+        assertEquals(valores[5], -1 );
+        assertEquals(valores[6], 0  );
+        assertEquals(valores[7], 9875551  );
+        assertEquals(valores[8], 9875555  );
+
     }
 }
