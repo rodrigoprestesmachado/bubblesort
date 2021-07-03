2b8676773ee4c2ba57c3b9176eb5f9726bea290d
Lucas <lucastovo@hotmail.com>
2021-07-02 23:47:57 -0300

    Fix: pmd errors and warnings

--- src/test/java/edu/ifrs/vvs/BubbleSortTests.java
+++ src/test/java/edu/ifrs/vvs/BubbleSortTests.java
@@ -1,29 +1,42 @@
 package edu.ifrs.vvs;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.Arrays;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for BubbleSortTests class.
+ */
 public class BubbleSortTests {
-  
-  BubbleSort bs = new BubbleSort();
 
-  @Test
-  public void assertOrderPositiveNumbers() {
-    int input[] = { 32, 4, 6, 90, 79 };
-    int expectedOutput[] = { 4, 6, 32, 79, 90 };
-    bs.sort(input);
-    assertTrue(Arrays.equals(expectedOutput, input));
-  }
+    private BubbleSort bs;
 
-  @Test
-  public void assertOrderNegativeNumbers() {
-    int input[] = { -32, -4, -6, -90, -79 };
-    int expectedOutput[] = { -90, -79, -32, -6, -4 };
-    bs.sort(input);
-    assertTrue(Arrays.equals(expectedOutput, input));
-  }
+    @BeforeEach
+    public void setup() {
+        bs = new BubbleSort();
+    }
+
+    @Test
+    @SuppressWarnings("checkstyle:magicnumber")
+    @DisplayName("Testando array de números positivos")
+    public void assertOrderPositiveNumbers() {
+        int[] input = {32, 4, 6, 90, 79};
+        int[] expectedOutput = {4, 6, 32, 79, 90};
+        bs.sort(input);
+        assertTrue(Arrays.equals(expectedOutput, input));
+    }
+
+    @Test
+    @SuppressWarnings("checkstyle:magicnumber")
+    @DisplayName("Testando array de números negativos")
+    public void assertOrderNegativeNumbers() {
+        int[] input = {-32, -4, -6, -90, -79};
+        int[] expectedOutput = {-90, -79, -32, -6, -4};
+        bs.sort(input);
+        assertTrue(Arrays.equals(expectedOutput, input));
+    }
 
 }
