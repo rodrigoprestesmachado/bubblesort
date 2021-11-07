e47283ffe8859bb1f9496d0abd85cdd723f24b8f
Paulo Serpa Antunes <pauloserpaantunes@gmail.com>
2021-11-07 12:25:37 -0300

    Add BubbleSortTest

---
+++ src/test/java/edu/ifrs/vvs/BubbleSortTest.java
@@ -0,0 +1,35 @@
+package edu.ifrs.vvs;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BubbleSortTest {
+
+  BubbleSort bubbleSort = new BubbleSort();
+  int[] values = {3, 2, 1};
+
+  @Test
+  @DisplayName("Certo: arrays igual e em ordem")
+  public void bubbleSortTestRight() {
+    bubbleSort.sort(values);
+    assertArrayEquals(new int[]{1, 2, 3}, values);
+  }
+
+  @Test
+  @DisplayName("Errado: array com um elemento a mais")
+  public void bubbleSortTestWrong1() {
+    assertFalse(Arrays.equals(new int[]{1, 2, 3, 4}, values));
+  }
+
+  @Test
+  @DisplayName("Errado: array desordenada")
+  public void bubbleSortTestWrong2() {
+    assertFalse(Arrays.equals(new int[]{3, 1, 2}, values));
+  }
+}
