db6dae10eddb9359914dad107f4cd16d6ca002b2
Nícolas De Conto <nicolasdeconto@hotmail.com>
2022-04-10 12:12:54 -0300

    Testes implementados para o metodo sort

--- src/test/java/edu/ifrs/vvs/AppTest.java
+++ src/test/java/edu/ifrs/vvs/AppTest.java
@@ -17,19 +17,58 @@
 
 package edu.ifrs.vvs;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-/**
- * Unit test for simple App.
- */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+
+    BubbleSort bolha = new BubbleSort();
+    static int[] array1 = new int[4];
+    static int[] array2 = new int[4];
+    static int[] array3 = new int[10];
+
+    @BeforeAll
+    public static void init() {
+        array1[0] = 6;
+        array1[1] = 9;
+        array1[2] = -5;
+        array1[3] = -5000;
+        array2[0] = 156842;
+        array2[1] = 156841;
+        array2[2] = 8;
+        array2[3] = 7;
+        array3[0] = 1;
+        array3[1] = 1;
+        array3[2] = 1;
+        array3[3] = 1;
+        array3[4] = 6;
+        array3[5] = 9;
+        array3[6] = 1;
+        array3[7] = 1;
+        array3[8] = 1;
+        array3[9] = 1;
+    }
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testApp1() {
+        int[] expectedResultArray1 = { -5000, -5, 6, 9 };
+        bolha.sort(array1);
+        assertArrayEquals(expectedResultArray1, array1);
+    }
+
+    @Test
+    void testApp2() {
+        int[] expectedResultArray2 = { 7, 8, 156841, 156842 };
+        bolha.sort(array2);
+        assertArrayEquals(expectedResultArray2, array2);
+    }
+
+    @Test
+    void testApp3() {
+        int[] expectedResultArray3 = { 1, 1, 1, 1, 1, 1, 1, 1, 6, 9 };
+        bolha.sort(array3);
+        assertArrayEquals(expectedResultArray3, array3);
     }
 }
