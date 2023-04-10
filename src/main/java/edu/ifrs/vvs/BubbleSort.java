5166709b718bb620884080a028a59330bdef6988
Kauana Henriques <karoltvbl@gmail.com>
2023-04-10 10:59:23 -0300

    Exercícios testes unitários

--- src/main/java/edu/ifrs/vvs/BubbleSort.java
+++ src/main/java/edu/ifrs/vvs/BubbleSort.java
@@ -50,7 +50,7 @@ public class BubbleSort {
             }
         }
     }
-
+    
     /**
      * Reads the values.
      *
--- src/test/java/edu/ifrs/vvs/AppTest.java
+++ src/test/java/edu/ifrs/vvs/AppTest.java
@@ -19,17 +19,91 @@ package edu.ifrs.vvs;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+import org.junit.jupiter.api.DisplayName;
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
+    private BubbleSort bubble = new BubbleSort();
     /**
      * Rigorous Test.
      */
     @Test
     void testApp() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    @DisplayName("Arrays com mesmos valores em ordens diferentes")
+    void testSort1(){
+        int [] valores = {4, 1, 9, 0};
+        int [] ordenados = {0, 1, 4, 9};
+
+        bubble.sort(valores);
+
+        assertArrayEquals(valores, ordenados);
+    }
+
+    @Test
+    @DisplayName("Arrays com valores diferentes")
+    void testSort2(){
+        int [] valores = {0, 1, 4, 9};
+        int [] ordenados = {7, 23, 9, 0};
+
+        bubble.sort(ordenados);
+
+        assertNotEquals(valores, ordenados);
+    }
+
+    @Test
+    @DisplayName("Arrays com valores iguais em ordens diferentes, poucos itens")
+    void testSort3(){
+        int [] valores = {9, 4, 1, 0};
+        int [] ordenados = {0, 1, 4, 9};
+        
+        bubble.sort(valores);
+
+        assertArrayEquals(valores, ordenados);
+    }
+
+    @Test
+    @DisplayName("Arrays com valores mais valores com ordenação correta")
+    void testSort4(){
+        int [] valores = {2, 34, 90, 200, 109866, 92929, 0, 1, 4, 9};
+        int [] ordenados = {0, 1, 2, 4, 9, 34, 90, 200, 92929, 109866};
+        
+        bubble.sort(valores);
+
+        assertArrayEquals(valores, ordenados);
+    }
+
+    @Test
+    @DisplayName("Arrays com valores mais valores não ordenados")
+    void testSort5(){
+        
+        int [] valores = {0, 1, 2, 4, 9, 34, 90, 200, 92929, 109866};
+        int [] ordenados = {2, 34, 90, 200, 109866, 92929, 0, 1, 4, 9};
+        
+        bubble.sort(valores);
+
+        assertArrayEquals(valores, ordenados);
+    }
+
+    @Test
+    @DisplayName("Arrays com mais valores ordenados")
+    void testSort6(){
+        
+        int [] valores = {2, 34, 90, 200, 109866, 92929, 0, 1, 4, 9};
+        int [] ordenados = {0, 1, 2, 4, 9, 34, 90, 200, 92929, 109866};
+        
+        bubble.sort(valores);
+
+        assertNotSame(valores, ordenados);
     }
 }
